3ae6797d10a5238f9934a69cea925ea2eb959993
joaovillas <joaopedrombvboas@gmail.com>
2018-04-27 15:31:40 -0300

    First Commit

---
+++ app/src/main/java/com/example/matheusvillas/myapplication/MainClass.kt
@@ -0,0 +1,12 @@
+package com.example.matheusvillas.myapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainClass : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_class)
+    }
+}
